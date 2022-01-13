47a4e59aef529c8cc50f2fc6816ebe0d2109dce1
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-01-13 12:08:08 +0530

    Add files via upload

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,13 +1,13 @@
 import  React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Profile from '../screens/Profile'
-import BottomTabNavigator from './BottomTabNavigator';
+import StackNavigator from './StackNavigator';
 const Drawer=createDrawerNavigator()
 const DrawerNavigator=()=>{
   return(
     <Drawer.Navigator>
       <Drawer.Screen name="Profile" componet={Profile}></Drawer.Screen>
-      <Drawer.Screen name="Home" componet={BottomTabNavigator}></Drawer.Screen></Drawer.Navigator>
+      <Drawer.Screen name="Home" componet={StackNavigator}></Drawer.Screen></Drawer.Navigator>
   )
 }
 
